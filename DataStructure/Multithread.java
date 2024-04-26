6a9c389d992cc68dc2a74650d7628e1f1d486777
Ramendra Pandey <khushbookushwaha43@gmail.com>
2024-04-26 23:42:25 +0530

    Initial commit

---
+++ DataStructure/Multithread.java
@@ -0,0 +1,11 @@
+package JAVA.DataStructure;
+
+public class Multithread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("Inside Run");
+        super.run();
+        
+    }
+}
